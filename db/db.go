445ae577f55a564e6af0ca94c0a0bf2b5e8dda41
takutotsuchie <t.tsuchie@buysell-technologies.com>
2023-04-12 11:51:23 +0900

    <feat> package dbのmutexの取り消し, hasuraでやっとmigrationファイルを作成する方法を見つけた

--- db/db.go
+++ db/db.go
@@ -12,16 +12,11 @@ var (
 
 // DBインスタンスをセット
 func SetDB(db *sql.DB) {
-	DBMutex.Lock()
-	defer DBMutex.Unlock()
 
 	DB = db
 }
 
 // DBインスタンスを取得
 func GetDB() *sql.DB {
-	DBMutex.Lock()
-	defer DBMutex.Unlock()
-
 	return DB
 }
